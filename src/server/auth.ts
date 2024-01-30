493c0f4fc197f48b4335ac76c3014e7488eed982
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-01-30 15:47:15 +0530

    next-auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -16,54 +16,21 @@ declare module "next-auth" {
 }
 
 const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
-console.log('process.env.GOOGLE_CLIENT_ID',process.env.GOOGLE_CLIENT_ID);
-console.log('process.env.GOOGLE_CLIENT_SECERET',process.env.GOOGLE_CLIENT_SECERET);
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECERET as string,
-    }),
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECERET ?? "",
+    })
   ],
   pages: {
     signIn: `/login`,
     verifyRequest: `/login`,
-    error: "/login", 
+    error: "/login", // Error code passed in query string as ?error=
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.user = user;
-      }
-      console.log('token', token);
-      return token;
-    },
-    session: async ({ session, token }) => {
-      try {
-        console.log('session', session);
-        console.log('token in session', token);
-        const userId = token.sub;
-        const userExists =(await db.user.findUnique({ where: { id: userId } })) || {};
-        console.log('userExists', userExists);
-        session.user = {
-          ...session.user,
-          ...userExists,
-          // @ts-expect-error
-          id: token.sub,
-          // @ts-expect-error
-          username: token?.user?.username || token?.user?.gh_username,
-        };
-        
-      } catch (error) {
-        console.log('error', error);
-      }
-      return session;
-
-    },
-  },
   cookies: {
     sessionToken: {
       name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
@@ -71,10 +38,49 @@ export const authOptions: NextAuthOptions = {
         httpOnly: true,
         sameSite: "lax",
         path: "/",
-        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
         domain: VERCEL_DEPLOYMENT? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`: undefined,
         secure: VERCEL_DEPLOYMENT,
       },
+    },
+  },
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      // const userId = token.sub;
+      // const userExists =(await prisma.user.findUnique({ where: { id: userId } })) || {};
+      // const googleAdsCredentials = await prisma.userGoogleCredentials.findMany({
+      //   where: { userId: token.sub },
+      // });
+
+      // if (googleAdsCredentials.length > 0) {
+      //   session.user = {
+      //     ...session.user,
+      //     ...userExists,
+      //     // @ts-expect-error
+      //     id: token.sub,
+      //     // @ts-expect-error
+      //     username: token?.user?.username || token?.user?.gh_username,
+      //     googleRefreshToken: googleAdsCredentials[0].refreshToken,
+      //     googleEmail: googleAdsCredentials[0].userGoogleEmail,
+      //   };
+      // } else {
+      //   session.user = {
+      //     ...session.user,
+      //     ...userExists,
+      //     // @ts-expect-error
+      //     id: token.sub,
+      //     // @ts-expect-error
+      //     username: token?.user?.username || token?.user?.gh_username,
+      //     googleRefreshToken: null,
+      //     googleEmail: null,
+      //   };
+      // }
+      return session;
     },
   },
 };
