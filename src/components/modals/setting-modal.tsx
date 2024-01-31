1dee211ccfb35c8aab829559229792f6c2fd3b09
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-01-31 12:17:55 +0530

    fixed the modal position

--- src/components/modals/setting-modal.tsx
+++ src/components/modals/setting-modal.tsx
@@ -29,7 +29,7 @@ export default function SettingModal({
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
-        <div className="inset-0 z-10 w-screen overflow-y-auto">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
