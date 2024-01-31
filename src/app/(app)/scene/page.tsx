614afc22a30bb349ec5b61afd96609237e0e36a2
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-01-31 12:15:12 +0530

    adding scene cards

--- src/app/(app)/scene/page.tsx
+++ src/app/(app)/scene/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import SceneCard from "~/components/scene/scene-card";
+import scene from "~/secene.json";
 function page() {
-  return <div>Scene Page</div>;
+  return (
+    <>
+      <div className="mx-auto my-2 grid max-w-7xl gap-4 sm:grid-cols-1 lg:grid-cols-4">
+        {scene.map((sceneCard) => {
+          return (
+            <div key={sceneCard.sceneTitle} className="p-[5px] lg:p-0">
+              <SceneCard />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default page;
