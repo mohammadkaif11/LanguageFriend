81a4db17e65e4c522c939ceaa1226b282ff3ea1a
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-02-05 20:59:06 +0530

    adding nav option

--- src/components/chat/text/ChatWindow.tsx
+++ src/components/chat/text/ChatWindow.tsx
@@ -8,18 +8,31 @@ import { startChart } from "~/server/chatGPT/chatgpt";
 import { type MessageInterface } from "model";
 import { useSession } from "next-auth/react";
 import { getFeedPrompt } from "~/server/chatGPT/PromptHelper";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 
 function ChatWindow() {
   const searchParams = useSearchParams();
   const session = useSession();
+  const [open, setOpen] = useState(false);
   const [isLearningMode, setIsLearningMode] = useState(false);
   const [messages, setMessages] = useState<MessageInterface[]>([]);
   const sceneId = searchParams?.get("sceneId");
   const characterId = searchParams?.get("characterId");
   const [loading, setLoading] = useState<boolean>(true);
-  const nativeLanguageCode = session?.data?.user?.nativeLanguageSetting? session?.data?.user?.nativeLanguageSetting: "en-US";
-  const targetLanguageCode = session?.data?.user?.targetLanguageSetting? session?.data?.user?.targetLanguageSetting: "en-US";
-  const chatPropmt=getFeedPrompt(sceneId,nativeLanguageCode,targetLanguageCode,isLearningMode);
+  const nativeLanguageCode = session?.data?.user?.nativeLanguageSetting
+    ? session?.data?.user?.nativeLanguageSetting
+    : "en-US";
+  const targetLanguageCode = session?.data?.user?.targetLanguageSetting
+    ? session?.data?.user?.targetLanguageSetting
+    : "en-US";
+  const chatPropmt = getFeedPrompt(
+    sceneId,
+    nativeLanguageCode,
+    targetLanguageCode,
+    isLearningMode,
+  );
 
   // useEffect(() => {
   //   const sendObj: MessageInterface = {
@@ -51,15 +64,62 @@ function ChatWindow() {
       }
       console.log("Last message:", lastMessage);
     }
-  }, [messages]); 
+  }, [messages]);
 
   return (
-    <div className="flex h-screen flex-row justify-between bg-white">
-      <div className="flex w-full mx-auto md:w-[550px] flex-col justify-between bg-gray-600 rounded-2xl">
+    <div className="flex h-screen flex-row justify-center bg-white">
+      <div className="flex w-full flex-col justify-between  bg-gray-600 md:w-[550px]">
         <div
           className="mt-5 flex flex-col overflow-y-scroll px-2"
           style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
         >
+          <Menu as="div" className="relative inline-block text-left md:hidden">
+            <div>
+              <Menu.Button>
+                {open ? (
+                  <XMarkIcon
+                    className="block h-6 w-6"
+                    aria-hidden="true"
+                    onClick={() => {
+                      setOpen(!open);
+                    }}
+                  />
+                ) : (
+                  <Bars3Icon
+                    className="block h-6 w-6"
+                    aria-hidden="true"
+                    onClick={() => {
+                      setOpen(!open);
+                    }}
+                  />
+                )}
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0  z-10  w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  <Menu.Item>
+                    <button className="block px-4 py-2 text-sm text-gray-700">
+                      Edit
+                    </button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button className="block px-4 py-2 text-sm text-gray-700">
+                      Delete
+                    </button>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
           {messages?.map((data: MessageInterface, index: number) => (
             <div key={index}>
               {data.role === "user" && <SenderTag text={data.content} />}
@@ -81,6 +141,53 @@ function ChatWindow() {
         </div>
         <InputFormTag setMessages={setMessages} chatHistory={messages} />
       </div>
+      <Menu as="div" className="relative hidden text-left md:inline-block">
+        <div>
+          <Menu.Button>
+            {open ? (
+              <XMarkIcon
+                className="block h-6 w-6"
+                aria-hidden="true"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              />
+            ) : (
+              <Bars3Icon
+                className="block h-6 w-6"
+                aria-hidden="true"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              />
+            )}
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0  z-10  w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                <button className="block px-4 py-2 text-sm text-gray-700">
+                  Edit
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button className="block px-4 py-2 text-sm text-gray-700">
+                  Delete
+                </button>
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   );
 }
