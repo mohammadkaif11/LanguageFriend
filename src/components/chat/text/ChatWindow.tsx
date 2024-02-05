4a20eac05d4177dd08af780bfd168e73e93e19ec
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-02-05 15:31:35 +0530

    remove toggle

--- src/components/chat/text/ChatWindow.tsx
+++ src/components/chat/text/ChatWindow.tsx
@@ -60,27 +60,6 @@ function ChatWindow() {
           className="mt-5 flex flex-col overflow-y-scroll"
           style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
         >
-          <div className="my-2 flex items-center justify-end">
-            <div className="flex">
-              <label className="relative mr-5 inline-flex cursor-pointer items-center">
-                <input
-                  type="checkbox"
-                  className="peer sr-only"
-                  checked={isLearningMode}
-                  readOnly
-                />
-                <div
-                  onClick={() => {
-                    setIsLearningMode(!isLearningMode);
-                  }}
-                  className="peer h-6 w-11 rounded-full bg-gray-200  after:absolute  after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-green-300"
-                ></div>
-                <span className="ml-2 text-sm font-medium text-gray-900">
-                  Learning Mode
-                </span>
-              </label>
-            </div>
-          </div>
           {messages?.map((data: MessageInterface, index: number) => (
             <div key={index}>
               {data.role === "user" && <SenderTag text={data.content} />}
--- src/components/scene/scene-card.tsx
+++ src/components/scene/scene-card.tsx
@@ -100,13 +100,34 @@ function SceneCard({
             dreams and inspire others.
           </p>
         </div>
-        <div className="p-6 pt-0">
+        <div className="flex p-6 pt-0">
           <button
             onClick={handleChat}
             className="flex select-none items-center gap-2 rounded-lg px-4 py-2 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             type="button"
           >
             Start Conversation
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              ></path>
+            </svg>
+          </button>
+          <button
+            onClick={handleChat}
+            className="flex select-none items-center gap-2 rounded-lg px-4 py-2 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button"
+          >
+            Learn Language
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
