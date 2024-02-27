30e755b969bd73380f59f12d098019bad01df0f1
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-02-27 18:32:14 +0530

    fix login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,18 +3,15 @@ import { Suspense } from "react";
 
 export default async function LoginPage() {
   return (
-    <div className="mx-5 border border-stone-200 py-10 dark:border-stone-700 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md">
-      {/* <Image
-        alt="Platforms Starter Kit"
-        width={100}
-        height={100}
-        className="relative mx-auto h-12 w-auto dark:scale-110 dark:rounded-full dark:border dark:border-stone-400"
-        src="/logo.png"
-      /> */}
-      <h1 className="mt-6 text-center font-cal text-3xl text-black">
-        Welcome to  Language Friend!
+    <div className="mx-5 border border-black bg-yellow-200 py-10 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md ">
+      <h4 className="text-2xl font-bold text-yellow-500 text-center">
+        <span className="text-4xl">L</span>ANGAUGE
+        <span className="text-4xl">F</span>RIEND
+      </h4>
+      <h1 className="font-cal mt-6 text-center text-3xl text-black">
+        Welcome to Language Friend!
       </h1>
-      <p className="mb-8 mt-2 text-center text-sm text-stone-600 dark:text-stone-400">
+      <p className="mb-8 mt-2 text-center text-sm text-black">
         Learning Language with Fun!
       </p>
       <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
